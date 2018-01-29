1382530bebad2a67a38c912ee422fd28de18dbca
Rein F <reinrijn@gmail.com>
2018-01-29 09:54:55 +0100

    Id is now 5 long

--- submit.php
+++ submit.php
@@ -26,7 +26,7 @@
             $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
             $charactersLength = strlen($characters);
             $randomString = '';
-            for ($i = 0; $i < 7; $i++) {
+            for ($i = 0; $i < 5; $i++) {
                 $randomString .= $characters[rand(0, $charactersLength - 1)];
             }
             return $randomString;
